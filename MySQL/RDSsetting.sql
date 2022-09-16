eca0e9d1614b93a9c3f4907d023b6a901ab333b1
nunomuratoshiaki <58365651+nunomuratoshiaki@users.noreply.github.com>
2022-09-16 21:33:37 +0900

    Add files via upload

---
+++ MySQL/RDSsetting.sql
@@ -0,0 +1,20 @@
+## RDSÚ‘±
+mysql -h xxx -u udemy -p
+
+## ƒf[ƒ^ì¬
+create database testdb;
+
+use testdb;
+
+create table testdb.account(id int, name varchar(20));
+
+show testdb;
+
+insert into account values (1001, "hiroyuki");
+
+insert into account values (1002, "shingo");
+
+insert into account values (1003, "naoko");
+
+
+select * from account;
